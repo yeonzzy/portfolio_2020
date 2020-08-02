1e27037b3465eeb9a843980d512aa661cb2cda69
yeonzzy <yeonji99@gmail.com>
2020-08-02 21:06:40 +0900

    EDIT script File

---
+++ js/section03.js
@@ -0,0 +1,32 @@
+var vibeBtn=$('.vibe-about>input[type=button]');
+
+            var sec3Go=$('.sec2-arrow');
+            var goTopSec3=$('.section.sec3').offset().top;
+            sec3Go.on('click',sec3GoFunc);
+            function sec3GoFunc(){
+                 $('html,body').animate({scrollTop:goTopSec3+1},1000);
+                 console.log(goTopSec3);
+            }
+
+            $(window).on('scroll', sec3_scrollFunc);
+            function sec3_scrollFunc(){
+                var goTopSec3=$('.section.sec3').offset().top;
+                var docScrollTop=$(document).scrollTop();
+                if(docScrollTop>=goTopSec3){
+                    $('.sec3-port').animate({left:'0'},2000);
+                    $('h1.vibeLogo').fadeIn(500,function(){
+                        $('.vibeColor>ul>li').fadeIn(1000);
+                    });
+                    $('h1.vibeLogo img').animate({opacity:'1'},1000);
+                    $('.vibe-about>h3').delay(1000).fadeIn(500);
+                    $('.vibe-about>h6').delay(1500).fadeIn(500);
+                    $('.vibe-about>span').delay(2000).fadeIn(500);
+                    $('.vibe-about>p').delay(2500).fadeIn(500);
+                    vibeBtn.delay(3000).fadeIn(500);
+                    $('.sec3-arrow').delay(3500).fadeIn(200);
+                }
+            }
+
+            vibeBtn.on('click',function(){
+                window.open('http://yeonzzy.dothome.co.kr/project_vibe/index.html');
+            });
---
+++ js/section05.js
@@ -0,0 +1,32 @@
+var appleBtn=$('.apple-about>input[type=button]');
+
+            var sec5Go=$('.sec4-arrow');
+            var goTopSec5=$('.section.sec5').offset().top;
+            sec5Go.on('click',sec5GoFunc);
+            function sec5GoFunc(){
+                 $('html,body').animate({scrollTop:goTopSec5+1},1000);
+                 console.log(goTopSec5);
+            }
+
+            $(window).on('scroll', sec5_scrollFunc);
+            function sec5_scrollFunc(){
+                var goTopSec5=$('.section.sec5').offset().top;
+                var docScrollTop=$(document).scrollTop();
+                if(docScrollTop>=goTopSec5){
+                    $('.sec5-port').animate({left:'0'},2000);
+                    $('h1.appleLogo').fadeIn(500,function(){
+                        $('.appleColor>ul>li').fadeIn(1000);
+                    });
+                    $('h1.appleLogo img').animate({opacity:'1'},1000);
+                    $('.apple-about>h3').delay(1000).fadeIn(500);
+                    $('.apple-about>h6').delay(1500).fadeIn(500);
+                    $('.apple-about>span').delay(2000).fadeIn(500);
+                    $('.apple-about>p').delay(2500).fadeIn(500);
+                    appleBtn.delay(3000).fadeIn(500);
+                    $('.sec5-arrow').delay(3500).fadeIn(200);
+                }
+            }
+
+            appleBtn.on('click',function(){
+                window.open('http://yeonzzy.dothome.co.kr/apple/apple.html');
+            });
